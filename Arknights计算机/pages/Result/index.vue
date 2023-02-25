f8be35f01d067ce47314798a2cedb2c8a798d5fc
zhenghanreal <124022189+zhenghanreal@users.noreply.github.com>
2023-02-25 17:18:50 +0800

    Add files via upload

---
+++ Arknights计算机/pages/Result/index.vue
@@ -0,0 +1,241 @@
+<template>
+	<view class="result">
+		<view class="type_card" v-for="item in result" :key="item.tag"><!-- 信息卡 -->
+			<view class="tag">
+				<text class="tag_text" v-for="tag in item.checkTag">{{tag}}</text>
+			</view>
+			<view ref="role" class="role_card" v-for="info in item.data" :key="info.name">
+				<view class="role_name">{{info.name}}</view>
+				<view class="role_content">
+					<view class="role_content_view" :style="{backgroundColor:info.color}">{{info.rank}}</view>
+					<view class="role_content_view">{{info.type[1]}}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import data1 from '../../static/data/data1.json'
+	import data2 from '../../static/data/data2.json'
+	export default{
+		data(){
+			return{
+				fiveS:data1,
+				SixS:data2,
+			}
+		},
+		props:{
+			values:{
+				type:Array,
+			}
+		},
+		// created() {
+		// 	//发送请求
+		// 	uni.request({
+		// 	    url:'../../static/data/data1.json', 
+		// 	    success: (res) => {
+		// 	        this.fiveS = res.data
+		// 	    }
+		// 	});
+		// 	uni.request({
+		// 	    url:'../../static/data/data2.json', 
+		// 	    success: (res) => {
+		// 	        this.SixS = res.data
+		// 	    }
+		// 	});
+		// },
+		computed:{
+			//过滤数据
+			result(){
+				//存放全部数据
+				const allData = []
+				//存放单次循环的数据
+				let eachData = []
+				//2组合标签合集
+				const doubleTag = []
+				//3组合标签合集
+				const tripleTag = []
+				//创建2组合标签
+				for(let i = 0; i<this.values.length; i++){
+					for(let j = i+1; j<this.values.length; j++){
+						doubleTag.push([this.values[i],this.values[j]])
+					}
+				}
+				//创建3组合标签
+				for(let i = 0; i<this.values.length; i++){
+					for(let j = i+1; j<this.values.length; j++){
+						for(let k = j+1; k<this.values.length; k++){
+								tripleTag.push([this.values[i],this.values[j],this.values[k]])
+						}
+					}
+				}	
+				//录入3组合标签(6星)
+				for(let i = 0; i<tripleTag.length; i++){
+					eachData = this.SixS.filter(item =>{
+						let check = false
+						if(tripleTag[i].includes("高级资深干员")){
+							if(JSON.parse(JSON.stringify(item)).type.includes(tripleTag[i][0])){
+								if(JSON.parse(JSON.stringify(item)).type.includes(tripleTag[i][1])){
+									if(JSON.parse(JSON.stringify(item)).type.includes(tripleTag[i][2])){
+										check = true
+									}
+								}
+							}
+						}				
+						return check
+					})
+					//获取多次循环的数据
+					if(eachData!=''){
+						allData.push({checkTag:tripleTag[i],data:eachData})
+					}
+				}
+				//录入2组合标签(6星)
+				for(let i = 0; i<doubleTag.length; i++){
+					eachData = this.SixS.filter(item =>{
+						let check = false
+						if(doubleTag[i].includes("高级资深干员")){
+							if(JSON.parse(JSON.stringify(item)).type.includes(doubleTag[i][0])){
+								if(JSON.parse(JSON.stringify(item)).type.includes(doubleTag[i][1])){
+									check = true
+								}
+							}
+						}
+						return check
+					})
+					//获取多次循环的数据
+					if(eachData!=''){
+						allData.push({checkTag:doubleTag[i],data:eachData})
+					}
+				}
+				//录入单标签(6星)
+				for(let i = 0; i<this.values.length; i++){
+					eachData = this.SixS.filter(item =>{
+						let check = false	
+						if(this.values[i].includes("高级资深干员")){
+							if(JSON.parse(JSON.stringify(item)).type[0].includes(this.values[i])){
+								check = true
+							}
+						}
+						return check
+					})
+					//获取多次循环的数据
+					if(eachData!=''){
+						allData.push({checkTag:[this.values[i]],data:eachData})
+					}
+				}
+				//录入3组合标签(5星)
+				for(let i = 0; i<tripleTag.length; i++){
+					eachData = this.fiveS.filter(item =>{
+						let check = false
+						if(JSON.parse(JSON.stringify(item)).type.includes(tripleTag[i][0])){
+							if(JSON.parse(JSON.stringify(item)).type.includes(tripleTag[i][1])){
+								if(JSON.parse(JSON.stringify(item)).type.includes(tripleTag[i][2])){
+									check = true
+								}
+							}
+						}
+						return check
+					})
+					//获取多次循环的数据
+					if(eachData!=''){
+						allData.push({checkTag:tripleTag[i],data:eachData})
+					}
+				}
+				//录入2组合标签(5星)
+				for(let i = 0; i<doubleTag.length; i++){
+					eachData = this.fiveS.filter(item =>{
+						let check = false
+						if(JSON.parse(JSON.stringify(item)).type.includes(doubleTag[i][0])){
+							if(JSON.parse(JSON.stringify(item)).type.includes(doubleTag[i][1])){
+								check = true
+							}
+						}
+						return check
+					})
+					//获取多次循环的数据
+					if(eachData!=''){
+						allData.push({checkTag:doubleTag[i],data:eachData})
+					}
+				}
+				//录入单标签(5星)	
+				for(let i = 0; i<this.values.length; i++){
+					eachData = this.fiveS.filter(item =>{
+						let check = false	
+						if(JSON.parse(JSON.stringify(item)).type.includes(this.values[i])){
+							check = true
+						}
+						return check
+					})
+					//获取多次循环的数据
+					if(eachData!=''){
+						allData.push({checkTag:[this.values[i]],data:eachData})
+					}
+				}
+				return allData
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.result{
+		padding: 10rpx;
+		background-color: #222a35;
+		height: auto;
+		display: flex;
+		flex-direction: column;
+		overflow: auto;
+		
+		.type_card{
+			width: 100%;
+			height: auto;
+			line-height: 70rpx;
+			background-color: #222a35;
+			box-sizing: border-box;
+			padding: 0 10rpx;
+			display: flex;
+			flex-wrap: wrap;
+			
+			.tag{
+				width: 100%;
+				
+				.tag_text{
+					border-radius: 5rpx;
+					padding: 8rpx 20rpx;
+					margin-left: 10rpx;
+					background-color: #0295de;
+					color: #eeeeee;
+				}
+			}
+			
+			.role_card{
+				border-radius: 10rpx;
+				font-size: 30rpx;
+				margin: 10rpx;
+				width: 30%;
+				height: 100rpx;
+				line-height: 45rpx;
+				background-color: #eeeeee;
+				box-sizing: border-box;
+				padding: 0 10rpx;
+				
+				.role_name{
+					font-weight: bold;
+					margin-left: 5rpx;
+				}
+				
+				.role_content{
+					display: flex;
+					
+					.role_content_view{
+						width: auto;
+						padding: 0rpx 10rpx;
+						border-radius: 10rpx;
+						margin: 0rpx 5rpx;
+					}
+				}
+			}
+		}
+	}
+</style>
---
+++ Arknights计算机/pages/index/index.vue
@@ -0,0 +1,201 @@
+<template>
+	<view class="index">
+		<view class="tag">
+			<view class="title">Tag列表</view>
+			<view class="divider"></view>
+			<view class="occupation"><!-- 职位 -->
+				<ul>
+					<li v-for="item in occupation" :key="item.id">
+						<button @click="checks(item)" ref="btn" :class="item.className">{{item.name}}</button>
+					</li>
+				</ul>
+			</view>
+			<view class="type"><!-- 功能 -->
+				<ul>
+					<li v-for="item in type" :key="item.id">
+						<button @click="checks(item)" ref="btn" :class="item.className">{{item.name}}</button>
+					</li>
+				</ul>
+			</view>
+			<view class="ability"><!-- 类型 -->
+				<ul>
+					<li v-for="item in ability" :key="item.id">
+						<button @click="checks(item)" ref="btn" :class="item.className">{{item.name}}</button>
+					</li>
+				</ul>
+			</view>
+			<view class="divider"></view>
+			<view class="reset">
+				<button @click="resetbtn()" class="reset_btn">重置</button>
+			</view>
+		</view>
+		<result :values="values"></result>
+	</view>
+</template>
+
+<script>
+	import Result from '../Result/index.vue'
+	export default {
+		data() {
+			return {
+				occupation:[
+					{check:true,className:"btn",name:'先锋',id:0},
+					{check:true,className:"btn",name:'近卫',id:1},
+					{check:true,className:"btn",name:'重装',id:2},
+					{check:true,className:"btn",name:'狙击',id:3},
+					{check:true,className:"btn",name:'术士',id:4},
+					{check:true,className:"btn",name:'医疗',id:5},
+					{check:true,className:"btn",name:'辅助',id:6},
+					{check:true,className:"btn",name:'特种',id:7},
+				],
+				type:[
+					{check:true,className:"btn",name:'新手',id:8},
+					{check:true,className:"btn",name:'资深干员',id:9},
+					{check:true,className:"btn",name:'高级资深干员',id:10},
+					{check:true,className:"btn",name:'远程位',id:11},
+					{check:true,className:"btn",name:'近战位',id:12},
+				],
+				ability:[
+					{check:true,className:"btn",name:'治疗',id:13},
+					{check:true,className:"btn",name:'支援',id:14},
+					{check:true,className:"btn",name:'输出',id:15},
+					{check:true,className:"btn",name:'群攻',id:16},
+					{check:true,className:"btn",name:'减速',id:17},
+					{check:true,className:"btn",name:'生存',id:18},
+					{check:true,className:"btn",name:'防护',id:19},
+					{check:true,className:"btn",name:'削弱',id:20},	
+					{check:true,className:"btn",name:'位移',id:21},	
+					{check:true,className:"btn",name:'爆发',id:22},	
+					{check:true,className:"btn",name:'控场',id:23},	
+					{check:true,className:"btn",name:'召唤',id:24},	
+					{check:true,className:"btn",name:'快速复活',id:25},	
+					{check:true,className:"btn",name:'费用回复',id:26},	
+					{check:true,className:"btn",name:'支援机械',id:27},	
+				],
+				num:0,
+				values:[],
+				flag:true
+				}
+		},
+		components:{
+			Result
+		},
+		methods:{
+			resetbtn(){
+				this.values = []
+				this.num = 0
+				for(let i=0; i<this.occupation.length; i++){
+					this.occupation[i].className = 'btn'
+					this.occupation[i].check = 'true'
+				}
+				for(let i=0; i<this.type.length; i++){
+					this.type[i].className = 'btn'
+					this.type[i].check = 'true'
+				}
+				for(let i=0; i<this.ability.length; i++){
+					this.ability[i].className = 'btn'
+					this.ability[i].check = 'true'
+				}
+			},
+			checks(item){
+				if(item.check){
+					if(this.num>=6){
+						alert("最多只能同时选择 6 个词条哦")
+						return
+					}
+					item.className = 'pickbtn'
+					item.check = false
+					this.num++
+					this.values.push(item.name)
+				}else{
+					item.className = 'btn'
+					item.check = true
+					this.num--
+					this.values = this.values.filter(value=>{
+						let check = true
+						if(value === item.name){
+							check = false
+						}
+						return check
+					})
+				}
+			},
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.index{
+		width: 100%;
+		height: 94vh;
+		background-color: #222a35;
+		
+		.tag{
+			height: auto;
+			position: relative;
+			
+			.divider{
+				background: #E0E3DA;
+				width: 100%;
+				height: 5rpx;
+			}
+			
+			.title{
+				font-size: 30rpx;
+				margin-left: 30rpx;
+				height: 60rpx;
+				line-height: 60rpx;
+				color: white;
+			}
+			
+			.occupation,
+			.ability,
+			.type{
+				ul{
+					padding: 5rpx 20rpx;
+					display: flex;
+					flex-wrap: wrap;
+					
+					li{
+						margin: 10rpx;
+						.btn{
+							color: black;
+							background-color: #eeeeee;
+							font-size: 26rpx;
+							width: auto;
+							height: 50rpx;
+							line-height: 50rpx;
+						}	
+						.pickbtn{
+							color: white;
+							background-color: #0295de;
+							font-size: 26rpx;
+							width: auto;
+							height: 50rpx;
+							line-height: 50rpx;
+						}	
+					}
+				}
+			}
+			
+			.reset{
+				position: relative;
+				width: 100%;
+				height: 60rpx;
+				padding: 15rpx;
+				
+				.reset_btn{
+					position: absolute;
+					left: 30rpx;
+					font-size: 30rpx;
+					width: 130rpx;
+					height: 60rpx;
+					line-height: 60rpx;
+					background-color: orangered;
+					color: white;
+				}
+			}
+		}
+		
+	}
+</style>
